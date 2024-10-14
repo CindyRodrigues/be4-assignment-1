b82b9edc97086f062bb19e98822da32a259aa996
CindyRodrigues <cindyrodrigues1999@gmail.com>
2024-10-14 16:04:50 +0530

    chore: adding CORS options

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
 const app = express()
 
 app.use(cors())
